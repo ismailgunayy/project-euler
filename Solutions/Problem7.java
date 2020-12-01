e0d3e26b48e8826f47ae5b664c9e094ba52e98fe
ismailgunayy <ismailggunay@gmail.com>
2020-12-01 21:18:14 +0300

    Added: Solution 7

---
+++ Solutions/Problem7.java
@@ -0,0 +1,40 @@
+/** 
+ *
+ * Problem 7
+ * What is the 10001st prime number?
+ * The Answer is 104743
+ *
+ * It is always open to improvement
+ * @author ismailgunayy
+ */
+
+public class Problem7 {
+
+	public static boolean isPrime(int number) {
+		if (number == 2) return true;
+		for (int divisor = 2; divisor <= (number / 2); divisor++) {
+			if (number % divisor == 0) return false;
+		}
+		return true;
+	}
+
+	public static int nthPrime(int n) {
+		
+		int count = 0;
+		int number = 2;
+		while (true) {	
+			if (isPrime(number)) {
+				count += 1;
+			}
+			if (count == 10001) break;
+			number += 1;
+		}
+		return number;
+	}
+
+	public static void main(String[] args) {
+			
+		System.out.println(nthPrime(10001));
+
+	}
+}
