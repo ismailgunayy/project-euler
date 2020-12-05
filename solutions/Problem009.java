75195072a6b17201907a64b1d61da460acb7eec1
ismailgunayy <29682307+ismailgunayy@users.noreply.github.com>
2020-12-05 18:16:12 +0300

    Modified: Solution 9

--- solutions/Problem009.java
+++ solutions/Problem009.java
@@ -1,6 +1,7 @@
 /**
  *
- * Problem 9 There exists exactly one Pythagorean triplet for which a + b + c = 1000
+ * Problem 9
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000
  * Find the product abc
  * The answer is 31875000
  *
