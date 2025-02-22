73d9b065b974cfa47e5c7af621fdce42629c2827
İsmail Günay <ismailggunay@gmail.com>
2025-02-22 16:27:50 +0100

    use recursion for problem 2

--- src/problems/problem-2.ts
+++ src/problems/problem-2.ts
@@ -7,23 +7,16 @@ import { measurePerformance, logResults } from '../metrics/performance';
  * fib (n + 3) = 4 * fib(n) + fib(n - 3)
  */
 function solve(): number {
-  function sumOfEvenValuedFibonacciNumbers(threshold: number) {
-    let sum = 0,
-      current = 2, // starting from fib(3) which is the first even number
-      previous = 0;
-
-    while (current < threshold) {
-      sum += current;
-
-      const next = 4 * current + previous;
-      previous = current;
-      current = next;
-    }
-
-    return sum;
+  function sumOfEvenFibs(
+    current: number,
+    previous: number,
+    threshold: number,
+  ): number {
+    if (current >= threshold) return 0;
+    return current + sumOfEvenFibs(4 * current + previous, current, threshold);
   }
 
-  return sumOfEvenValuedFibonacciNumbers(4000000);
+  return sumOfEvenFibs(2, 0, 4000000);
 }
 
 if (require.main === module) {
