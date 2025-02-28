4ba79e96fd1a0958c2a1f5c6776e94cd4ff49acb
İsmail Günay <ismailggunay@gmail.com>
2025-02-28 17:57:03 +0100

    validate argument in run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,4 +3,8 @@
 mode=$1
 number=$2
 
-ts-node src/problems/problem-$number.ts $mode
+if [[ -n "$number" && "$number" =~ ^[0-9]+$ ]]; then
+    ts-node src/problems/problem-$number.ts $mode
+else
+    echo "Error: Please provide a valid number as the argument."
+fi
