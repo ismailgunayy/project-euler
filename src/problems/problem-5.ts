e355bbe03e878a644ab386f41938553b6e21f7e4
İsmail Günay <ismailggunay@gmail.com>
2025-03-01 17:54:58 +0100

    naming

--- src/problems/problem-5.ts
+++ src/problems/problem-5.ts
@@ -6,47 +6,51 @@ import { measurePerformance, logResults } from '../metrics/performance';
 export default function solve(): number {
 	type TPowersOfFactors = { [key: string]: number };
 
-	function findPrimeFactors(num: number): TPowersOfFactors {
-		const factorsOfNum: TPowersOfFactors = {};
+	function findPowersOfPrimeFactors(num: number): TPowersOfFactors {
+		const result: TPowersOfFactors = {};
 		let factor = 2;
 
 		while (num !== 1) {
 			if (num % factor === 0) {
-				if (factorsOfNum[factor]) factorsOfNum[factor]++;
-				else factorsOfNum[factor] = 1;
+				if (result[factor]) result[factor]++;
+				else result[factor] = 1;
 
 				num /= factor;
-			} else factor += factor === 2 ? 1 : 2;
+			} else {
+				factor += factor === 2 ? 1 : 2;
+			}
 		}
 
-		return factorsOfNum;
+		return result;
 	}
 
-	const powersOfFactors: TPowersOfFactors = {};
+	const powersOfPrimeFactors: TPowersOfFactors = {};
 
 	for (let i = 20; i > 1; i--) {
-		const result = findPrimeFactors(i);
+		const result = findPowersOfPrimeFactors(i);
 
 		Object.keys(result).forEach((factor: string) => {
-			if (powersOfFactors[factor]) {
-				if (powersOfFactors[factor] < result[factor]) {
-					powersOfFactors[factor] = result[factor];
+			const object = powersOfPrimeFactors;
+
+			if (object[factor]) {
+				if (object[factor] < result[factor]) {
+					object[factor] = result[factor];
 				}
 			} else {
-				powersOfFactors[factor] = result[factor];
+				object[factor] = result[factor];
 			}
 		});
 	}
 
-	let result = 1;
+	let smallestPositive = 1;
 
-	for (let factor in powersOfFactors) {
+	for (let factor in powersOfPrimeFactors) {
 		const factorNum = Number(factor);
 
-		result *= factorNum ** powersOfFactors[factor];
+		smallestPositive *= factorNum ** powersOfPrimeFactors[factor];
 	}
 
-	return result;
+	return smallestPositive;
 }
 
 if (require.main === module) {
