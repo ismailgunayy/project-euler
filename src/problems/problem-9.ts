35724ef30d3447114af8dd7ea90535a5d4c79c80
İsmail Günay <ismailggunay@gmail.com>
2025-03-06 13:45:14 +0100

    optimise problem 9

--- src/problems/problem-9.ts
+++ src/problems/problem-9.ts
@@ -4,14 +4,14 @@ import { measurePerformance, logResults } from '../metrics/performance';
  * Problem 9: Find the product of a * b * c, in which a^2 + b^2 = c^2 and a + b + c = 1000
  */
 export default function solve(): number {
-	const limit = 1000;
+	const PERIMETER = 1000;
 
-	for (let i = 0; i < limit / 3; i++) {
-		for (let j = i; j < (limit - i) / 2; j++) {
-			for (let k = j; k < limit / 2; k++) {
-				if (i + j + k === limit && i ** 2 + j ** 2 === k ** 2) {
-					return i * j * k;
-				}
+	for (let i = 0; i < PERIMETER / 3; i++) {
+		for (let j = i; j < (PERIMETER - i) / 2; j++) {
+			const k = PERIMETER - i - j;
+
+			if (i ** 2 + j ** 2 === k ** 2) {
+				return i * j * k;
 			}
 		}
 	}
