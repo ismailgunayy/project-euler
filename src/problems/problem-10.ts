a8cbce8989c5b43a5c14acde904edf7cebf6f343
İsmail Günay <ismailggunay@gmail.com>
2025-03-09 13:25:57 +0100

    optimise problem 10

--- src/problems/problem-10.ts
+++ src/problems/problem-10.ts
@@ -2,12 +2,14 @@ import { measurePerformance, logResults } from '../metrics/performance';
 
 /**
  * Problem 10: Sum of all prime numbers below 2,000,000
+ * Optimisation steps:
+ * 1 - In isPrime function, increase the iterator by 2 every step, since every even numbers have '2' in them.
  */
 export default function solve(): number {
 	function isPrime(number: number): boolean {
 		if (number === 2) return true;
 
-		for (let i = 2; i <= Math.sqrt(number); i++) {
+		for (let i = 3; i <= Math.sqrt(number); i += 2) {
 			if (number % i === 0) {
 				return false;
 			}
